7b6bae7f2912869e7d6875ee363eb16f8b3a626d
Colby Cocking <cocking_08@wsu.edu>
2021-06-16 21:33:03 -0700

    Init repo

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.postgresql", "postgresql", "42.2.18")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
